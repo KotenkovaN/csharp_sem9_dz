0f01b83dd72208f6a107528c76f173f0be2cb90e
Nataly <natalya-kotenkova@yandex.ru>
2022-12-20 17:09:37 +0100

    added task68

---
+++ task68/Program.cs
@@ -0,0 +1,16 @@
+﻿int A(int m, int n ) 
+{ 
+      if (m == 0) 
+          return (n + 1);
+     else if ((m != 0) && (n == 0))
+          return A( m - 1, 1); 
+      else  
+          return A( m - 1, A(m, n-1));
+}
+Console.Clear(); 
+Console.Write("Введите число m: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число n: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(A(m, n)); 
+
