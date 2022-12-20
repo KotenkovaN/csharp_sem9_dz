6c4861d1ae71ff1a571bdb6c2d61baf1e450d13f
Nataly <natalya-kotenkova@yandex.ru>
2022-12-20 16:13:54 +0100

    added task66

---
+++ task66/Program.cs
@@ -0,0 +1,12 @@
+﻿int rec(int m, int n ) 
+{ 
+  if (n == m ) 
+      return n;
+  return rec( m, n - 1) + n;        
+}
+Console.Clear(); 
+Console.Write("Введите число m: "); 
+int m = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число n: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(rec(m, n)); 
